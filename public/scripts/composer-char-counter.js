df901e9f18f31d87aa63478ae69cf76e16e1cd6d
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-04-25 14:44:43 -0700

    create variables for cleaner code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,19 +1,18 @@
 $(document).ready(function() {
   // --- our code goes here ---
-  $('#tweet-tweet').on('keyup', function() {
-    
+  $('#tweet-text').on('input', function() {
     //count input length
-    const count = $(this).val().length;
-
+    const input = $(this).val().length;
+    const maximumInput = 140;
+    let remaining = maximumInput - input;
     //find the counter
-    const counter = $(this).parent().children('.counter');
-    let remaining = 140 - count;
+    const $counter = $(this).parent().children('.counter');
 
-    counter.text(remaining);
+    $counter.text(remaining);
     if(remaining < 0) {
-      counter.css('color', 'red');
+      $counter.css('color', 'red');
     } else {
-      counter.css('color', '#545149');
+      $counter.css('color', '#545149');
     }
   });
 });
